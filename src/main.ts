eb5cfc0f19f87ec48e3081f9aac5370fc6cb6df2
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-06 01:29:38 +0300

    chore: env prot, global validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT);
 }
 bootstrap();
