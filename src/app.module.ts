8a9e1e6b9cb364f9894352ef49a6c9b5b64df07c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-06 01:29:17 +0300

    chore: imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService],
 })
