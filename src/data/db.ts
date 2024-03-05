c1bf22fea1007495bd9653b5c66c6f77df6f647c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-06 01:29:45 +0300

    chore: in-memory db

---
+++ src/data/db.ts
@@ -0,0 +1,5 @@
+import { Artist } from 'src/artist/entities/artist.entity';
+import { User } from 'src/user/entities/user.entity';
+
+export const users: User[] = [];
+export const artists: Artist[] = [];
