e820025db151f07c06d7bb0b9dde14e483ad1cbe
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-07 15:17:28 +0300

    chore: imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
-  imports: [UserModule, ArtistModule],
+  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],
   controllers: [AppController],
   providers: [AppService],
 })
