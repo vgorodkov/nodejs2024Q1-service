52a641d35717b3ae139498b38a35c9a3fe6614ea
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-07 16:57:10 +0300

    chore: .env variables and swagger docs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],
+  imports: [
+    UserModule,
+    ArtistModule,
+    TrackModule,
+    AlbumModule,
+    FavsModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const apiYamlPath = join(__dirname, '../doc', 'api.yaml');
+  const apiYaml = readFileSync(apiYamlPath, 'utf8');
+
+  const apiJson = yaml.load(apiYaml) as OpenAPIObject;
+
+  SwaggerModule.setup('api', app, apiJson);
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
