60d93d5f7fc34931f73a0fcc8f2fbfad06ffb388
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-03-15 19:44:35 +0300

    chore: transform date to number

--- src/user/user.interceptor.ts
+++ src/user/user.interceptor.ts
@@ -1,27 +1,37 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
 
 @Injectable()
 export class UserInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
- 
-    return next.handle().pipe(map(data=>{
+    return next.handle().pipe(
+      map((data) => {
+        if (!data) {
+          return;
+        }
 
-      if(!data){
-        return
-      }
+        if (!Array.isArray(data)) {
+          const { password, ...noPassUser } = data;
+          noPassUser.createdAt = new Date(noPassUser.createdAt).getTime();
+          noPassUser.updatedAt = new Date(noPassUser.updatedAt).getTime();
 
-      if(!Array.isArray(data)){
-        const { password, ...noPassUser } = data;
-        return noPassUser;
-      }
+          return noPassUser;
+        }
 
-      return data.map(user=>{
-        const {password,...noPassUser} = user
-        return noPassUser
-      })
-    }));
+        return data.map((user) => {
+          const { password, ...noPassUser } = user;
+          noPassUser.createdAt = new Date(noPassUser.createdAt).getTime();
+          noPassUser.updatedAt = new Date(noPassUser.updatedAt).getTime();
+
+          return noPassUser;
+        });
+      }),
+    );
   }
 }
